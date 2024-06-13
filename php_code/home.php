dde2d8e6cd449d963d5ba6f171e032e8fe362a13
nazimhilman <beltboy322@gmail.com>
2024-06-13 21:26:06 +0800

    Login test, not sure if can use without xampp

---
+++ php_code/home.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+    ?>
+
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+
+
+    <title>HOME</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <h1>Hello, <?phph echo $SESSION['user_name']; ?></h1>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
+
+<?php
+}
+else{
+    header("Location: index.php");
+    exit();
+
+}
+?>
